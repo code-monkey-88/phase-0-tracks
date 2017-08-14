c756e0043de35a8e8fe0c4dfa316c7e434275c8e
Youssef Kholeif <yykholeif@gmail.com>
2017-08-14 14:54:41 +0200

    updated hamster id generator program by converting age strings to floats

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,18 +1,29 @@
+puts "Welcome to the Hamster id-generator"
+
 puts "what is your hamsters name?"
-hamster_name=gets.chomp
+  hamster_name=gets.chomp
 
 puts "what is your hamsters volume level?"
-volume_level=gets.chomp
+  volume_level=gets.chomp
 
 puts "what is your hamster's fur color?"
-fur_color=gets.chomp
+  fur_color=gets.chomp
 
 puts "is the hamster a good candidate for adoption?"
-adoption_score=gets.chomp
+  adoption=gets.chomp
 
 puts "what is your hamster's estimated age?"
-age=gets.chomp
+  age=gets.chomp
 
-if age==""
-age=nil
-end
+  if age==""
+    age=nil
+  else
+    age=age.to_f
+  end
+
+puts "Hamster id-card:"
+puts "hamster name is #{hamster_name}"
+puts "voume level is #{volume_level}"
+puts "fur color is #{fur_color}"
+puts "adopt? #{adoption}"
+puts "estimated age #{age}"
