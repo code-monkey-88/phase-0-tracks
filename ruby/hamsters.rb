91d5b1ec4471028419fa0c1ffe3ca2a421bd5c9a
Youssef Kholeif <yykholeif@gmail.com>
2017-08-14 14:25:24 +0200

    created program to gather data on hamsters of unknown origin

---
+++ ruby/hamsters.rb
@@ -0,0 +1,18 @@
+puts "what is your hamsters name?"
+hamster_name=gets.chomp
+
+puts "what is your hamsters volume level?"
+volume_level=gets.chomp
+
+puts "what is your hamster's fur color?"
+fur_color=gets.chomp
+
+puts "is the hamster a good candidate for adoption?"
+adoption_score=gets.chomp
+
+puts "what is your hamster's estimated age?"
+age=gets.chomp
+
+if age==""
+age=nil
+end
