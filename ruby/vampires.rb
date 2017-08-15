454cc8eb840898fafefc4dcc7870a5f8c80b7979
Youssef Kholeif <yykholeif@gmail.com>
2017-08-15 20:29:11 +0200

    added loop to filter for allergies

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,6 +12,7 @@ puts "what is your name?"
 
 puts "How old are you?"
   age=gets.chomp
+  age=age.to_i
 
 puts "What year were you born?"
   dob=gets.chomp
@@ -25,31 +26,31 @@ puts "Would you like to enroll in the company’s health insurance? (y/n)"
   insurance_wanted=gets.chomp
 
 end_list="done"
+end_list_2="sunshine"
 puts "name any allergies, one at a time (type done when finished)"
   allergies=gets.chomp
 
- #incorporate break statement to include sunshine
-    while allergies != end_list
+#not sure how to skip directly to the result of “Probably a vampire for "sunshine" input”
+    while allergies != end_list && allergies !=end_list_2
       puts "list allergies: "
       allergies = gets.chomp
     end
 
- #logic not precise enough + order matters
- #perhaps need to use ! &/or case statement
-    if age==dob && (garlic_wanted=="y" || insurance_wanted=="y")
+ #still not sure why order is so important for logic here!
+    if name=="Drake Cula" || name=="Tu Fang"
+      vampire="definately"
+    elsif  age==dob && (garlic_wanted=="y" || insurance_wanted=="y")
       vampire="probably not"
-    elsif age!=dob && (garlic_wanted=="n" || insurance_wanted=="n")
-      vampire="probably"
-    elsif allergies="sunshine"
-      vampire="probably"
     elsif age!=dob && garlic_wanted=="n" && insurance_wanted=="n"
       vampire="almost certainly"
-    elsif name=="Drake Cula" || name=="Tu Fang"
-      vampire="definately"
+    elsif age!=dob && (garlic_wanted=="n" || insurance_wanted=="n")
+      vampire="probably"
     else
       puts "Results inconclusive"
     end
-    puts "#{name} is #{vampire} a vampire"
+      puts "#{name} is #{vampire} a vampire"
 
   count=count+1
 end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
