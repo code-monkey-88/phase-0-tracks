df042f6bf43765667bb1db9ec568ace0b8802f61
Youssef Kholeif <yykholeif@gmail.com>
2017-08-24 14:31:03 +0200

    pseudo-coded grocery list generator program

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,48 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+  # [convert input into hash key]
+  # set default quantity
+  # print the list to the console [p grocery_list]
+# output: [hash]
+
+
+# Method to add an item to a list
+# input: list (item name, and optional quantity)
+# steps:
+  # print hash
+  # ask user if they would like to add any additional items to the list?
+  # set default quantity
+  # use a loop to allow addition of unknown amount of items
+  # conditional statements to initialise or break loop
+  # print hash - debugging
+# output: [updated hash]
+
+# Method to remove an item from the list
+# input: [updated hash]
+# steps:
+  # print hash
+  # ask user if they would like to delete an item from the list
+  # wrap above in a loop to allow deletion of unknown amount of items
+  # conditional statement to initialise or break loop
+  # print hash - debugging
+# output: [trimmed hash]
+
+# Method to update the quantity of an item
+# input: [hash]
+# steps:
+  # print hash with default quantities
+  # would you like to update the quantity of items on your list?
+  # conditional statement to intialise or break loop
+  # prompt user requesting value you would like to update?
+  # request new quantity for that specific value
+  # print hash - debugging
+# output: [final hash]
+
+# Method to print a list and make it look pretty
+# input: [final hash]
+# steps:
+  # consider capitalising final result or any other asthetic modification
+  # iterate through the hash to print item and quantity on separate lines
+  # method that takes a hash or each.do
+# output: list of individual items and their quantities line-by-line
