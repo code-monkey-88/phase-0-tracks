e2572d973796d10d254d5ab365759a93918e2cd1
Youssef Kholeif <yykholeif@gmail.com>
2017-08-25 21:23:50 +0200

    set up Puppy class with in-built methods and iterated through them using driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,75 @@
 class Puppy
 
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(integer)
+    puts "woof" * integer
+    integer
+  end
+
+  def roll_over
+    puts "roll over"
+  end
+
+  def dog_years(integer)
+    dog_age = 7 * integer
+    dog_age
+  end
+
+  def sleep(integer)
+    puts "ZzZ" * integer
+    integer
+  end
+
 end
 
+#driver code to initialise
+truffles = Puppy.new
+truffles.fetch('ball')
+truffles.speak(3)
+truffles.roll_over
+truffles.dog_years(7)
+truffles.sleep(3)
+bubbles = Puppy.new
+
+
+class Gymnast
+
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+
+  def flip(integer)
+    puts "flip" * integer
+    integer
+  end
+
+  def jump(name)
+    puts "how high #{name}?"
+    name
+  end
+
+end
+
+
+index = 0
+gymnast_array = []
+
+until index == 50
+  gymnast_array << Gymnast.new
+  index +=1
+end
+
+p gymnast_array
+
+gymnast_array.each do |person|
+  person.jump('Jimmy')
+  person.flip(3)
+end
