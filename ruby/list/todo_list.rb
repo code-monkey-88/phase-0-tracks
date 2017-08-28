e98bd7b3b2e14caaf868ac9f9ad2f5525d777b89
Youssef Kholeif <yykholeif@gmail.com>
2017-08-28 18:42:14 +0200

    running rspec test across all methods

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,7 +1,23 @@
 class TodoList
 
+  def initialize(items_array)
+    @initial_list = items_array
+  end
+
   def get_items
-    list = ["do the dishes", "mow the lawn"]
+    @initial_list
+  end
+
+  def add_item(item)
+    @initial_list << item
+  end
+
+  def delete_item(item)
+    @initial_list.delete(item)
+  end
+
+  def get_item(index)
+    @initial_list[index]
   end
 
 end
