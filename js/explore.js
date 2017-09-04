301bcdb29c0a83cde12ad198880ac1832a720fc7
Youssef Kholeif <yykholeif@gmail.com>
2017-09-04 14:36:49 +0200

    fixed bug

---
+++ js/explore.js
@@ -0,0 +1,25 @@
+// Reverse a string
+//steps;
+//measure string length
+//create a loop that counts backwards
+//set index value to length of string
+//print values
+
+
+var reversedString = "";
+var index = 0
+
+function reverse(word) {
+  for (var i = word.length - 1; i >= index; i--) {
+     console.log(word[i]);
+     reversedString = reversedString + word[i];
+  }
+ return reversedString
+}
+
+reverse("Youssef");
+console.log(reversedString)
+
+if (1==1) {
+	console.log("here is your reversed string" + reversedString);
+}
