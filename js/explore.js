92c051fce119bb0fa231541479488d992296ee64
Youssef Kholeif <yykholeif@gmail.com>
2017-09-06 14:10:44 +0200

    minused 1 from length to return final value in array

--- js/explore.js
+++ js/explore.js
@@ -1,19 +1,25 @@
 // Reverse a string
 //steps;
 //measure string length
-//create a loop that counts until length is reached
-//set index value to length of string and count backwards
+//create a loop that counts backwards
+//set index value to length of string
+//print values
+
+
+var reversedString = "";
+var index = 0
 
 function reverse(word) {
-  var index = 0;
-  for (var i = word.length-1; i >= index; i--) {
+  for (var i = word.length - 1; i >= index; i--) {
      console.log(word[i]);
+     reversedString = reversedString + word[i];
   }
-  return console.log(word[i]);
+ return reversedString
 }
 
-var reverse_name = reverse("Youssef");
+reverse("Youssef");
+console.log(reversedString)
 
 if (1==1) {
-	console.log("here is your reversed string" + reverse_name);
+	console.log("here is your reversed string --> " + reversedString);
 }
