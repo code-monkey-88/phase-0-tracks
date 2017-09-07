6768140b14eaeadfd3d47832b177dbc7d5a992a4
Youssef Kholeif <yykholeif@gmail.com>
2017-09-07 20:26:26 +0200

    fixed bugs in release 0

--- js/algos.js
+++ js/algos.js
@@ -1,37 +1,25 @@
 Function to find the longest phrase
 input: array of words or phrases
 steps:
-        iterate over each item within array
-        measure length of string within each item
-        //sort items from largest to smallest length (conditional statement?)
-// output: return item with the longest length
+        //iterate over each item within array
+        //measure length of string within each item
+        //create empty string
+        //set empty string to new item if length is greater
+        // output: return item with the longest length
 
 
-var arrayWords = ["long phrase","longest phrase","longer phrase"];
-var arrayWordsLength = [];
-var length;
 
-function measureItemLength(arrayWords) {
-  for (var i in arrayWords) {
-    // arrayWordsLength.push(arrayWords[i].length);
-    // arrayWordsLength.sort();
-    // console.log(arrayWordsLength);
+  var phrases = ["long phrase","longest phrase","longer phrase"];
 
-    arrayWords.sort();
-    length = arrayWords[i].length;
-    console.log(length)
+  function findLongestPhrase(phrases) {
+    var longestPhrase = "";
+
+    for (var i=0; i < phrases.length; i++) {
+      if (phrases[i].length > longestPhrase.length) {
+        longestPhrase = phrases[i];
+      }
     }
-}
-
-measureItemLength(arrayWords);
-// console.log(arrayWordsLength);
-
-var longWords = ["long phrase","longest phrase","longer phrase"]
-var longest = 0;
-for (var i = 0; i < longWords.length; i++) {
-if (longest.length < longWords[i].length) {
-    longest = longWords[i];
+    return longestPhrase;
   }
-}
 
-console.log(longest)
+  console.log(findLongestPhrase(phrases))
